97c27781e1e063239eb7d967870979334fe04906
SatwikTellakula <119725034+SatwikTellakula@users.noreply.github.com>
2023-01-05 10:00:32 +0530

    git commit -m "commit message"

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,40 @@
 // Write your code here
+import './index.css'
+
+const LatestMatch = props => {
+  const {latestMatch} = props
+  const {
+    umpires,
+    result,
+    manOfTheMatch,
+    id,
+    date,
+    venue,
+    competingTeam,
+    competingTeamLogo,
+    firstInnings,
+    secondInnings,
+    matchStatus,
+  } = latestMatch
+  return (
+    <div className="latest-match">
+      <div className="competing-container">
+        <h1>{competingTeam}</h1>
+        <p>{date}</p>
+        <p>{venue}</p>
+        <p>{result}</p>
+      </div>
+      <img src={competingTeamLogo} className="competing-team" />
+      <div className="innings-container">
+        <p>First Innings</p>
+        <p>{firstInnings}</p>
+        <p>Second Innings</p>
+        <p>{secondInnings}</p>
+        <p>Man Of the Match</p>
+        <p>{manOfTheMatch}</p>
+        <p>Umpires</p>
+        <p>{umpires}</p>
+      </div>
+    </div>
+  )
+}
