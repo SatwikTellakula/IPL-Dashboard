23a02b61c926ca140f14618467e70f8729faa840
SatwikTellakula <119725034+SatwikTellakula@users.noreply.github.com>
2023-01-05 10:10:11 +0530

    git commit  -m "commit message"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import './App.css'
 
 import Home from './components/Home'
+import TeamCard from './components/TeamCard'
 
-const App = () => <Home />
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/ipl/:id" component={TeamCard} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
