7203cc3a0a5a3760cd9b3afd7a47528149e00e42
SatwikTellakula <119725034+SatwikTellakula@users.noreply.github.com>
2023-01-05 11:11:30 +0530

    git commit -m "commit message"

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css'
 
 import Home from './components/Home'
 import TeamCard from './components/TeamCard'
+import TeamMatches from './components/TeamMatches'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/ipl/:id" component={TeamCard} />
+      <Route path="/ipl/:id" component={TeamMatches} />
     </Switch>
   </BrowserRouter>
 )
